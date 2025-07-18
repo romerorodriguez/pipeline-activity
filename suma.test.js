534e61b8f919c537899b2d61334f6efbc66b911e
romerorodriguez <a3522110182@alumno.uttehuacan.edu.mx>
2025-07-18 16:05:18 -0600

    nuevo test

--- suma.test.js
+++ suma.test.js
@@ -3,3 +3,7 @@ const suma = require('./suma');
 test('adds 1 + 2 to equal 3', () => {
   expect(suma(1, 2)).toBe(3);
 });
+
+test('adds 1 + 9 to equal 10', () => {
+  expect(suma(1, 9)).toBe(10);
+});
