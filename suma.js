e8fee3e8106a413071120c649b294f4a4a49a52c
romerorodriguez <a3522110182@alumno.uttehuacan.edu.mx>
2025-07-18 16:43:38 -0600

    cambios

--- suma.js
+++ suma.js
@@ -1,4 +1,5 @@
-function suma(a, b) {
-  return a + b;
+function suma(a,b) {
+    return a + b;
 }
+
 module.exports = suma;
--- suma.test.js
+++ suma.test.js
@@ -1,9 +1,26 @@
-const suma = require('./suma');
+const suma = require('./suma'); 
 
-test('adds 1 + 2 to equal 3', () => {
-  expect(suma(1, 2)).toBe(3);
-});
+function test(desc, fn) {
+    try {
+        fn();
+        console.log("✅" + desc);
+    } catch (error) {
+        console.error("❌" + desc);
+        console.error("" + error.message);
+        process.exitCode = 1;
+    }
+}
 
-test('adds 1 + 9 to equal 10', () => {
-  expect(suma(1, 9)).toBe(10);
-});
+function assertEquals(actual, esperado) {
+    if (actual !== esperado) {
+        throw new Error(`Esperado: ${esperado}, pero obtuvo ${actual}`);
+    }
+}
+
+test("Suma 2 + 2 = 4", () => {
+    assertEquals(suma(3,0),3);
+}); 
+
+test("Suma 3 + 3 = 6", () => {
+    assertEquals(suma(3,3),6);
+}); 
